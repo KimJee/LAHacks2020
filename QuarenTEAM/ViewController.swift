e47fa23e826f8d161bfd33f1a0b3d16fb50a5094
Jee Hoon Kim <jeehoonkim@Jees-Mac.local>
2020-03-28 14:22:39 -0700

    Baseline Code

---
+++ QuarenTEAM/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  QuarenTEAM
+//
+//  Created by Jee Hoon Kim on 3/28/20.
+//  Copyright © 2020 Cashew Natalia & Jee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	// Jee's comment 14:21 3/28/20
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
